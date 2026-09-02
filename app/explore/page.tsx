b2d4b7758a91365cfca5fc806006055c520a4664
AuronixCommerce <42s28mpnn6@privaterelay.appleid.com>
2026-09-02 07:51:16 +0500

    feat: route explore to enhanced discovery

--- app/explore/page.tsx
+++ app/explore/page.tsx
@@ -1,1 +1,1 @@
-import {FeaturePage} from '@/components/ex/feature-page';export default function Page(){return <FeaturePage kind="explore"/>}
+import {EnhancedExplore} from '@/components/ex/enhanced-explore';export default function Page(){return <EnhancedExplore/>}
